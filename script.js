e701d80fa2d098b1fc1acf8ae2c41c87a3283afb
amon-wanyonyi <amon.wanyonyi@student.moringaschool.com>
2021-11-20 16:19:45 +0300

    add changes to index

--- script.js
+++ script.js
@@ -1,39 +1,38 @@
 //development-image
 $(document).ready(function() {
     $("#development-image").click(function() {
-        $("#development-image").slideDown('1500').hide('1000');
-        $("#development").show('1500');
+        $("#development-image").slideDown("1500").hide("1000");
+        $("#development").show("1500");
     });
     $("#development").click(function() {
-        $("#development").slideUp('1500');
-        $("#development-image").slideDown('1500');
+        $("#development").slideUp("1500");
+        $("#development-image").slideDown("1500");
     });
 });
 
 //design-image
 $(document).ready(function() {
     $("#design-image").click(function() {
-        $("#design-image").slideDown('1500').hide('1000');
-        $("#design").show('1500');
+        $("#design-image").slideDown("1500").hide("1000");
+        $("#design").show("1500");
     });
     $("#design").click(function() {
-        $("#design").slideUp('1500');
-        $("#design-image").slideDown('1500');
+        $("#design").slideUp("1500");
+        $("#design-image").slideDown("1500");
     });
 });
 // product-image
 $(document).ready(function() {
     $("#product-image").click(function() {
-        $("#product-image").slideDown('1500').hide('1000');
-        $("#product").show('1500');
+        $("#product-image").slideDown("1500").hide("1000");
+        $("#product").show("1500");
     });
     $("#product").click(function() {
-        $("#product").slideUp('1500');
-        $("#product-image").slideDown('1500');
+        $("#product").slideUp("1500");
+        $("#product-image").slideDown("1500");
     });
 });
 // Hover effect - hide and show
-
 
 $(document).ready(function() {
     $("#work").mouseover(function() {
@@ -46,7 +45,7 @@ $(document).ready(function() {
 
     $("#work1").mouseover(function() {
         $("#mouseenter1").show();
-    })
+    });
     $("#work1").mouseout(function() {
         $("#mouseenter1").hide();
     });
@@ -54,42 +53,42 @@ $(document).ready(function() {
 
     $("#work2").mouseover(function() {
         $("#mouseenter2").show();
-    })
+    });
     $("#work2").mouseout(function() {
         $("#mouseenter2").hide();
     });
     //image 4
     $("#work3").mouseover(function() {
         $("#mouseenter3").show();
-    })
+    });
     $("#work3").mouseout(function() {
         $("#mouseenter3").hide();
     });
     //image 5
     $("#work4").mouseover(function() {
         $("#mouseenter4").show();
-    })
+    });
     $("#work4").mouseout(function() {
         $("#mouseenter4").hide();
     });
     //image 6
     $("#work5").mouseover(function() {
         $("#mouseenter5").show();
-    })
+    });
     $("#work5").mouseout(function() {
         $("#mouseenter5").hide();
     });
     //image 7
     $("#work6").mouseover(function() {
         $("#mouseenter6").show();
-    })
+    });
     $("#work6").mouseout(function() {
         $("#mouseenter6").hide();
     });
     //image 8
     $("#work7").mouseover(function() {
         $("#mouseenter7").show();
-    })
+    });
     $("#work7").mouseout(function() {
         $("#mouseenter7").hide();
     });
@@ -102,7 +101,6 @@ function myMessage() {
     var email = document.getElementById("email").value;
     var message = document.getElementById("comment").value;
     if (email !== "" && name !== "" && message !== "") {
-
         alert(
             "Hello" +
             " " +
