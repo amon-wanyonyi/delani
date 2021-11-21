395b1a8f51975695a245633da018e9a13f9af0df
amon-wanyonyi <amon.wanyonyi@student.moringaschool.com>
2021-11-21 12:43:16 +0300

    Add README.md file, screenshots, and changed the images paths

---
+++ README.md
@@ -0,0 +1,30 @@
+# DELANI - STUDIO
+## Author
+#### Amon Wanyonyi
+## Description
+##### This project isa web application for Delani studio that gives the details of what services offered there.
+##### The site has information about the studio, a portfolio section and a form through which you can send <br> message to the Delani studio management.
+## Screenshots
+![Screenshot](images/Screenshot.png)
+![Screenshot2](images/Screenshot2.png)
+![Screenshot3](images/Screenshot3.png)
+## Live link for Delani studio :
+[Click the link to Delani Studio Website](https://amon-wanyonyi.github.io/delani/)
+## Installation Instructions
+* Ctrl+Alt+T to open the terminal
+* cd to the directory you want the delani directory to be located
+* git clone https://github.com/amon-wanyonyi/delani
+* cd delani
+* Open with your favorite IDE
+  ## Technologies Used
+  - HTML5
+  - JQuery
+  - JavaScript
+  - CSS
+  - Bootstrap
+  - Markdown
+  ## Known Bugs
+  Bugs with the mail chimp but was fixed
+  ## Contact Information
+  For questions, to hire me or contribution, please contact me: <br>
+  xxx110680@gmail.com 
---
+++ script.js
@@ -0,0 +1,116 @@
+//development image
+$(document).ready(function () {
+  $("#development-image").click(function () {
+    $("#development-image").slideDown('1500').hide('1000');
+    $("#development").show('1500');
+  });
+  $("#development").click(function () {
+    $("#development").slideUp('1500');
+    $("#development-image").slideDown('1500');
+  });
+});
+
+//design
+$(document).ready(function () {
+  $("#design-image").click(function () {
+    $("#design-image").slideDown('1500').hide('1000');
+    $("#design").show('1500');
+  });
+  $("#design").click(function () {
+    $("#design").slideUp('1500');
+    $("#design-image").slideDown('1500');
+  });
+});
+// product
+$(document).ready(function () {
+  $("#product-image").click(function () {
+    $("#product-image").slideDown('1500').hide('1000');
+    $("#product").show('1500');
+  });
+  $("#product").click(function () {
+    $("#product").slideUp('1500');
+    $("#product-image").slideDown('1500');
+  });
+});
+// hide and show the hover effect
+
+
+$(document).ready(function () {
+  $("#work").mouseover(function () {
+    $("#mouseenter").show();
+  });
+  $("#work").mouseout(function () {
+    $("#mouseenter").hide();
+  });
+  //image 2
+
+  $("#work1").mouseover(function () {
+    $("#mouseenter1").show();
+  })
+  $("#work1").mouseout(function () {
+    $("#mouseenter1").hide();
+  });
+  //image 3
+
+  $("#work2").mouseover(function () {
+    $("#mouseenter2").show();
+  })
+  $("#work2").mouseout(function () {
+    $("#mouseenter2").hide();
+  });
+  //image 4
+  $("#work3").mouseover(function () {
+    $("#mouseenter3").show();
+  })
+  $("#work3").mouseout(function () {
+    $("#mouseenter3").hide();
+  });
+  //image 5
+  $("#work4").mouseover(function () {
+    $("#mouseenter4").show();
+  })
+  $("#work4").mouseout(function () {
+    $("#mouseenter4").hide();
+  });
+  //image 6
+  $("#work5").mouseover(function () {
+    $("#mouseenter5").show();
+  })
+  $("#work5").mouseout(function () {
+    $("#mouseenter5").hide();
+  });
+  //image 7
+  $("#work6").mouseover(function () {
+    $("#mouseenter6").show();
+  })
+  $("#work6").mouseout(function () {
+    $("#mouseenter6").hide();
+  });
+  //image 8
+  $("#work7").mouseover(function () {
+    $("#mouseenter7").show();
+  })
+  $("#work7").mouseout(function () {
+    $("#mouseenter7").hide();
+  });
+});
+
+//validating name , email and message inputs not to be empty.
+
+function myMessage() {
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("comment").value;
+  if (email !== "" && name !== "" && message !== "") {
+   
+    alert(
+      "Hello" +
+      " " +
+      name +
+      " we have received your message. Thank you for reaching out to us."
+    );
+  } else if (email == "" || name !== "" || message !== "") {
+    alert("Please fill in all the details");
+  }
+}
+
